68230c875793d88aa4c75d57b470482886cc198c
Vishal <vishalpanchal1828vp@gmail.com>
2024-07-23 03:53:19 +0530

    Fix cors error

--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -1,11 +1,16 @@
+import express, { Application } from "express";
 import * as dotenv from "dotenv";
 import { initServer } from "./app";
+import cors from "cors";
+const app: Application = express();
 
 dotenv.config();
 
+app.use(cors());
+
 async function init() {
-  const app = await initServer();
-  app.listen(8000, () => console.log(`Server Started at PORT:8000`));
+    const app = await initServer();
+    app.listen(8000, () => console.log(`Server Started at PORT:8000`));
 }
 
 init();
